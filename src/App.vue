a7a386975939cf615d2cc3853b664a1a2d2ab10e
smallbin <3057988675@qq.com>
2023-02-26 16:54:30 +0800

    第四天

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   <div class="app">
     <router-view/>
     <tab-bar v-if="!route.meta.hideTabBar"/>
-    <!-- <tab-bar/> -->
+    <loading/>
   </div>
 </template>
 
 <script setup>
 
 import TabBar from "@/components/tab-bar/tab-bar.vue"
+import Loading from "@/components/loading/loading.vue"
 import { useRoute } from "vue-router";
 
 const route = useRoute()
